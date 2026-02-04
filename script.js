dd2aced2f718e03bae861eb2db1405acefd45dba
Tory Stodard <125704757+Tory-Stodard@users.noreply.github.com>
2026-02-04 08:20:44 -0700

    Added canvas-confetti library via cdn. Added confetti effect to yes-btn.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function clickedYes() {
+  confetti({
+    particleCount: 150,
+    spread: 50,
+    startVelocity: 35,
+    colors: ['#ffffff', '#ebc7c7', '#e57272', '#e14545', '#dcc6c6', '#e71414'],
+  });
+}
+
 function clickedNo() {
   alert("Yeah, you're not really my type anyways.");
 }
